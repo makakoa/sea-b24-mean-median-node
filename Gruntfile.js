eb80c69e012ee64a1731a49207f3e70ae7639795
makakoa <makakoa15@gmail.com>
2014-12-07 18:50:39 -0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,25 @@
+/*jshint node: true*/
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-jscs');
 
   grunt.initConfig({
     jshint: {
-      all: ["lib/*.js"]
+      all: ['lib/*.js']
     },
 
     simplemocha: {
       src: ['test/**/*.js']
+    },
+
+    jscs: {
+      src: ['lib/*.js'],
+      options: {
+        config: '.jscsrc'
+      }
     }
   });
-  grunt.registerTask('test', ['jshint', 'simplemocha']);
+  grunt.registerTask('test', ['jshint', 'simplemocha', 'jscs']);
   grunt.registerTask('default', ['test']);
-}
+};
